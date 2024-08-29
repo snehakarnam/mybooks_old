49dbe65e149af3c6f6b1a8cdea1c0c22ef79289c
Sneha.Karnam <sneha karnam>
2024-08-30 01:45:58 +0530

    skeleton

---
+++ subscription-all/src/test/java/subscription/SubscriptionAllApplicationTests.java
@@ -0,0 +1,13 @@
+package subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ subscription-entryway/src/main/java/com/mybook/resource/SubscriptionResource.java
@@ -0,0 +1,4 @@
+package com.mybook.resource;
+
+public class SubscriptionResource {
+}
